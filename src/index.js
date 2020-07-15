2d062b448f1d5d00213f06616604acfbce328bc3
tana-gh <yozoranotakaku@gmail.com>
2020-07-15 13:27:05 +0900

    Add eslint settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import App from './components/App.svelte'
 import '../assets/scss/style.scss'
 
-const app = new App({
-    target: document.getElementById('app')
-})
+new App({ target: document.getElementById('app') })
